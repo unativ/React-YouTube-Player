c7d9fadc96a6a2c6b30bff433503da9c989c239c
uri.nativ <uri.nativ@klarna.com>
2016-01-17 08:35:04 +0200

    Lecture 28 Video Selection

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -8,7 +8,6 @@ const VideoDetail = ({video}) => {
   const videoId = video.id.videoId;
   const url = `https://www.youtube.com/embed/${videoId}`;
 
-    console.log(video);
   return (
     <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
